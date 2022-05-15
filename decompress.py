80522d01ee092e8ba53e72f0e3aa300da5074a19
lordjuacs <juacsr@gmail.com>
2022-05-14 22:19:01 -0500

    pca avance?

--- decompress.py
+++ decompress.py
@@ -2,14 +2,14 @@ import os
 import bz2
 import shutil
 
-path = '.'
+path = "."
 
 for path, dirs, files in os.walk(path):
     for filename in files:
         basename, ext = os.path.splitext(filename)
-        if ext.lower() != '.bz2':
+        if ext.lower() != ".bz2":
             continue
         fullname = os.path.join(path, filename)
         newname = os.path.join(path, basename)
-        with bz2.open(fullname) as fh, open(newname, 'wb') as fw:
+        with bz2.open(fullname) as fh, open(newname, "wb") as fw:
             shutil.copyfileobj(fh, fw)
--- train.py
+++ train.py
@@ -5,6 +5,9 @@ import time
 
 from imageio import imread
 from sklearn import svm
+from sklearn.decompostion import PCA
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 N_SAMPLES = 10
 
@@ -17,21 +20,21 @@ def generate_C1(path):
     vectors = []
     for file in os.listdir(path):
         if file.endswith(".tif"):
-            ti = get_image(path + '/' + file)
+            ti = get_image(path + "/" + file)
             for file2 in os.listdir(path):
                 if file2 != file and file2.endswith(".tif"):
-                    tj = get_image(path + '/' + file2)
+                    tj = get_image(path + "/" + file2)
                     vectors.append(ti - tj)
     return vectors
 
 
 def chooseDirectory():
     idx = rd.randint(1, 1012)
-    dir = '/data/images/' + str(idx).zfill(5)
+    dir = "/data/images/" + str(idx).zfill(5)
     if idx < 740:
-        dir = 'gray_feret_cd1' + dir
+        dir = "gray_feret_cd1" + dir
     else:
-        dir = 'gray_feret_cd2' + dir
+        dir = "gray_feret_cd2" + dir
     # check if dir exists
     if not os.path.exists(dir):
         return chooseDirectory()
@@ -44,8 +47,8 @@ def C2_helper(image_path, ti):
         dir = chooseDirectory()
         person_imgs = os.listdir(dir)
         for img_path in person_imgs:
-            if img_path.endswith(".tif") and 'fa' in img_path:
-                tj = get_image(dir + '/' + img_path)
+            if img_path.endswith(".tif") and "fa" in img_path:
+                tj = get_image(dir + "/" + img_path)
                 vectors.append(ti - tj)
     return vectors
 
@@ -53,10 +56,26 @@ def C2_helper(image_path, ti):
 def generate_C2(path):
     vectors = []
     for file in os.listdir(path):
-        if file.endswith(".tif") and 'fa' in file:
-            ti = get_image(path + '/' + file)
+        if file.endswith(".tif") and "fa" in file:
+            ti = get_image(path + "/" + file)
             vectors += C2_helper(path, ti)
     return vectors
+
+
+def generate_PCA(dataset):
+    train_img, test_img, train_lbl, test_lbl = train_test_split(
+        dataset.data, dataset.imgname, test_size=0.2, random_state=0
+    )
+    scaler = StandardScaler()
+    scaler.fit(train_img)
+    train_img = scaler.transform(train_img)
+    test_img = scaler.transform(test_img)
+
+    pca = PCA(0.95)  # mantener 95% de varianza en el dataset, pca elige el n_components
+    pca.fit(train_img)
+    train_img = pca.transform(train_img)
+    test_img = pca.transform(test_img)
+    # ya se redujo la dimensionalidad
 
 
 def train(path, clf):
@@ -85,12 +104,12 @@ def train(path, clf):
     print("Fitted in {} seconds".format(end - start))
 
 
-if __name__ == '__main__':
-    clf = svm.SVC(kernel='linear', C=1)
-    basePath = '/data/images/'
-    for dir in os.listdir('gray_feret_cd1' + basePath):
+if __name__ == "__main__":
+    clf = svm.SVC(kernel="linear", C=1)
+    basePath = "/data/images/"
+    for dir in os.listdir("gray_feret_cd1" + basePath):
         print("Training on {}".format(dir))
-        train('gray_feret_cd1' + basePath + dir, clf)
-    for dir in os.listdir('gray_feret_cd2' + basePath):
+        train("gray_feret_cd1" + basePath + dir, clf)
+    for dir in os.listdir("gray_feret_cd2" + basePath):
         print("Training on {}".format(dir))
-        train('gray_feret_cd2' + basePath + dir, clf)
+        train("gray_feret_cd2" + basePath + dir, clf)
